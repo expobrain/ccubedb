c56bfe32f3c70fff2af025a3a227fb059e98bdbd
Vladimir Kazanov <vladimir.kazanov@corp.badoo.com>
2018-05-09 13:30:16 +0100

    Cubedb cube partition visitor

--- src/cdb_cubedb.h
+++ src/cdb_cubedb.h
@@ -5,6 +5,8 @@
 
 typedef struct cdb_cubedb cdb_cubedb;
 
+typedef void cdb_cubedb_partition_visitor_function(sds cube_name, sds partition_name, cdb_partition *partition);
+
 cdb_cubedb *cdb_cubedb_create(void);
 void cdb_cubedb_init(cdb_cubedb *cubedb);
 void cdb_cubedb_destroy(cdb_cubedb *cubedb);
@@ -13,6 +15,8 @@ cdb_cube *cdb_cubedb_find_cube(cdb_cubedb *cubedb, const sds name);
 bool cdb_cubedb_add_cube(cdb_cubedb *cubedb, sds cube_name, cdb_cube *cube);
 bool cdb_cubedb_del_cube(cdb_cubedb *cubedb, sds cube_name);
 char **cdb_cubedb_get_cube_names(cdb_cubedb *cubedb, size_t *cube_count);
+
+void cdb_cubedb_for_each_cube_partition(cdb_cubedb *cubedb, cdb_cubedb_partition_visitor_function cube_visitor);
 
 /* TODO: delete a cube */
 
--- tests/cubedb-test.c
+++ tests/cubedb-test.c
@@ -76,10 +76,81 @@ char *test_add_delete_cube()
     return 0;
 }
 
+char *test_for_each_simple()
+{
+    cdb_cubedb *cubedb = cdb_cubedb_create();
+    defer { cdb_cubedb_destroy(cubedb); }
+
+    cdb_cube *cube1 = cdb_cube_create();
+    cdb_cubedb_add_cube(cubedb,"test cube1", cube1);
+
+    {
+        cdb_insert_row *irow = cdb_insert_row_create("part1", 1);
+        defer { cdb_insert_row_destroy(irow); }
+        cdb_insert_row_add_column_value(irow, "column1", "val1") ;
+        cdb_cube_insert_row(cube1, irow);
+    }
+
+    {
+        cdb_insert_row *irow = cdb_insert_row_create("part2", 1);
+        defer { cdb_insert_row_destroy(irow); }
+        cdb_insert_row_add_column_value(irow, "column1", "val1") ;
+        cdb_cube_insert_row(cube1, irow);
+
+    }
+
+    cdb_cube *cube2 = cdb_cube_create();
+    cdb_cubedb_add_cube(cubedb,"test cube2", cube2);
+
+    {
+        cdb_insert_row *irow = cdb_insert_row_create("part3", 1);
+        defer { cdb_insert_row_destroy(irow); }
+        cdb_insert_row_add_column_value(irow, "column1", "val1") ;
+        cdb_cube_insert_row(cube2, irow);
+    }
+
+    {
+        cdb_insert_row *irow = cdb_insert_row_create("part4", 1);
+        defer { cdb_insert_row_destroy(irow); }
+        cdb_insert_row_add_column_value(irow, "column1", "val1") ;
+        cdb_cube_insert_row(cube2, irow);
+
+    }
+
+    uint64_t partition_count = 0;
+    bool cube1_part1 = false;
+    bool cube1_part2 = false;
+    bool cube2_part3 = false;
+    bool cube2_part4 = false;
+
+    void cube_visitor(sds cube_name, sds partition_name, cdb_partition * partition)
+    {
+        (void) partition;
+        partition_count++;
+        if (0 == strcmp(cube_name, "cube1") && 0 == strcmp(partition_name, "part1"))
+            cube1_part1 = true;
+        if (0 == strcmp(cube_name, "cube1") && 0 == strcmp(partition_name, "part2"))
+            cube1_part2 = true;
+        if (0 == strcmp(cube_name, "cube2") && 0 == strcmp(partition_name, "part3"))
+            cube2_part3 = true;
+        if (0 == strcmp(cube_name, "cube2") && 0 == strcmp(partition_name, "part4"))
+            cube2_part4 = true;
+    }
+
+    cdb_cubedb_for_each_cube_partition(cubedb, cube_visitor);
+
+    mu_assert("Wrong partition count", 4 == partition_count);
+    mu_assert("Cube partitions not found", !cube1_part1 || !cube1_part2 || !cube2_part3 || !cube2_part4);
+
+    return 0;
+}
+
+
 static char *all_tests()
 {
     mu_run_test(test_find_cube);
     mu_run_test(test_add_delete_cube);
+    mu_run_test(test_for_each_simple);
     return 0;
 }
 
